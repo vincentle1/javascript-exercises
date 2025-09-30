03a6ba0c7574d8f4417a3b36859e06a1d9385b4a
Vincent Le <vincentle@vincents-mbp.mynetworksettings.com>
2025-09-29 22:41:24 -0400

    Complete leap years exercise - implement leap year logic with proper century year handling

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            }
+            else return false;
+        }
+        else return true;
+    }  
+    else return false;
 };
 
+console.log(leapYears(1600));
+/* First, identify if the leap year is divisible by 4. Then determine whether it's 
+divisible by 100 UNLESS it's also divisible by 400. If it IS, then go on to check whether it's
+divisible by 100.
+&& (year % 100 == 0 && year % 400 == 0) */
 // Do not edit below this line
 module.exports = leapYears;
