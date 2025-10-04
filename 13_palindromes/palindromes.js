c0bf0b78910eb0bfcbb309134e6146169af7243b
Vincent Le <vincentle@vincents-mbp.mynetworksettings.com>
2025-10-03 22:16:41 -0400

    :)

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let clean = str.toLowerCase().split('').filter(char => /[a-z0-9]/.test(char));
+    let cleanString = clean.join('');
+    let reverseString = clean.reverse().join('');
+
+    if (cleanString == reverseString) {
+        return true;
+    }
+    else {
+        return false;
+    }
 
 };
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes; 
